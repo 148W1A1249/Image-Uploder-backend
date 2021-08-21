ed0fea47bfd5b47386c2b7b422eb16e367385a1d
khasim sharif shaik <khasimsharif30@gmail.com>
2021-08-21 11:35:57 +0530

    cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 var cors = require('cors');
 const { cloudinary } = require('./utils/cloudinary');
 
+app.use(cors())
 app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({linit: '50mb', extended:true}));
 
